754191a8576d55672ed3e4be0084d987f7a5c8b3
蜜圪节 <604892907@qq.com>
2017-12-20 09:43:54 +0800

    update zipkin

---
+++ zipkin-server/src/main/java/the/spring/cloud/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,15 @@
+package the.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+}
