317f119adc2cf4324d8fe55691122800c714b0b0
houjianguang <houjguang@163.com>
2022-07-09 22:50:20 +0800

    jackson时区与null处理

--- microservice-utils/src/main/java/the/spring/cloud/utils/common/JsonUtils.java
+++ microservice-utils/src/main/java/the/spring/cloud/utils/common/JsonUtils.java
@@ -1,9 +1,23 @@
 package the.spring.cloud.utils.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.TimeZone;
+
 public class JsonUtils {
 
-    public static String toJsonString(Object obj) {
+    public static String toJsonString(Object obj){
+        String result = "";
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        try {
+            result = mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
 
-        return "";
+        }
+        return result;
     }
 }
